31cad554c11e24a629db8432f606820961cec741
Tom Etzel <thomas.etzel@gmail.com>
2022-06-09 12:12:32 -0500

    updating to use layout

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,17 +1,8 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>My Blog</title>
-</head>
-<body>
-<article>
-    <h1><?= $post->title; ?></h1>
-    <div><?= $post->body; ?></div>
-</article>
+@extends('layout')
 
-</body>
-</html>
+@section('content')
+    <article>
+        <h1>{{$post->title}}</h1>
+        <div>{!! $post->body !!}</div>
+    </article>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,14 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>My Blog</title>
-</head>
-<body>
-<?php foreach ($posts as $post) : ?>
-<article>
-    <h1>
-        <a href="/posts/<?= $post->slug; ?>">
-            <?= $post->title; ?>
-        </a>
-    </h1>
-    <div><?= $post->excerpt; ?></div>
-</article>
-<?php endforeach; ?>
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{$post->slug}}">
+                    {{$post->title}}
+                </a>
+            </h1>
+            <div>{{$post->excerpt}}</div>
+        </article>
+    @endforeach
+@endsection
